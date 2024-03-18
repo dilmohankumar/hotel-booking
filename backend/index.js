4c237c7c34a36abe0592e7671a1ab573881024ae
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2024-03-19 01:18:02 +0530

    Initial commit

--- backend/index.js
+++ backend/index.js
@@ -12,20 +12,19 @@ import userRouter from "./routes/users.js";
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 4000;
-const corsOption = {
+
+// Define CORS options
+const corsOptions = {
     origin: 'https://hotel-booking-6uqnc4xix-dilmohans-projects.vercel.app', // Update with your frontend URL
-    credentials: false
+    credentials: true // If you're using cookies or authorization headers, set this to true
 };
 
-// Rest of your code remains the same...
-
-
+// Connect to MongoDB
 const connect = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URL, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-           
         });
         console.log("MongoDB connected successfully");
     } catch (error) {
@@ -33,20 +32,19 @@ const connect = async () => {
     }
 };
 
-
+// Middleware
 app.use(express.json());
-app.use(cors(corsOption));
+app.use(cors(corsOptions)); // Apply CORS middleware
 app.use(cookieParser());
 
+// Routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/tour", tourRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/reviews", reviewRouter);
 app.use("/api/v1/booking", bookingRouter);
 
-
-
-
+// Start the server
 app.listen(port, async () => {
     try {
         await connect();
