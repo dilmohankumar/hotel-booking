887e08c162249f354ad801b39fe778796a81a833
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2024-03-24 21:35:18 +0530

    Initial commit

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,4 +1,4 @@
 // // utils/config.js
 // export const BASE_URL = "http://localhost:4000/api/v1";
-export const BASE_URL = "https://hotel-booking-production-94ed.up.way.app/api/v1";
+export const BASE_URL = "https://hotel-booking-production-4135.up.railway.app/api/v1";
 
